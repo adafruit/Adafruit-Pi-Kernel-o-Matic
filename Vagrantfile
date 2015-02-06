f65a42cfff68db3ca9e2f9ebfc8b6e630b2de990
Todd Treece <toddtreece@gmail.com>
2015-02-06 13:32:29 -0500

    symlink adabuild to the new location of build.sh

--- Vagrantfile
+++ Vagrantfile
@@ -35,7 +35,6 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   end
 
-  config.vm.synced_folder "scripts", "/kernel_builder", owner: "root", group: "root"
   config.vm.provision "shell", path: "provision.sh"
 
 end
--- provision.sh
+++ provision.sh
@@ -25,5 +25,5 @@ apt-get update
 apt-get install -y git unzip build-essential libncurses5-dev
 
 if [ ! -f /usr/sbin/adabuild ]; then
-  ln -s /kernel_builder/build.sh /usr/sbin/adabuild
+  ln -s /vagrant/build.sh /usr/sbin/adabuild
 fi
