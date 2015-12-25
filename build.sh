c9df7d7712b022d542d36fba8a44d1bc01136c75
Todd Treece <toddtreece@gmail.com>
2015-12-25 12:41:59 -0500

    make dtbs, and copy them to /boot

--- build.sh
+++ build.sh
@@ -166,9 +166,9 @@ ARCH=arm CROSS_COMPILE=${CCPREFIX} make menuconfig
 echo "**** SAVING A COPY OF YOUR v1 CONFIG TO /vagrant/v1_saved_config ****"
 cp .config /vagrant/v1_saved_config
 echo "**** COMPILING v1 KERNEL ****"
-ARCH=arm CROSS_COMPILE=${CCPREFIX} make -j${NUM_CPUS} -k
+ARCH=arm CROSS_COMPILE=${CCPREFIX} make -j${NUM_CPUS} -k zImage modules dtbs
 ARCH=arm CROSS_COMPILE=${CCPREFIX} INSTALL_MOD_PATH=${MOD_DIR} make -j${NUM_CPUS} modules_install
-${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/Image $PKG_DIR/boot/kernel.img
+${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel.img
 cp -r ${MOD_DIR}/lib/* ${PKG_DIR}
 
 # RasPi v2 build
@@ -188,9 +188,12 @@ ARCH=arm CROSS_COMPILE=${CCPREFIX} make menuconfig
 echo "**** SAVING A COPY OF YOUR v2 CONFIG TO /vagrant/v2_saved_config ****"
 cp .config /vagrant/v2_saved_config
 echo "**** COMPILING v2 KERNEL ****"
-ARCH=arm CROSS_COMPILE=${CCPREFIX} make -j${NUM_CPUS} -k
+ARCH=arm CROSS_COMPILE=${CCPREFIX} make -j${NUM_CPUS} -k zImage modules dtbs
 ARCH=arm CROSS_COMPILE=${CCPREFIX} INSTALL_MOD_PATH=${MOD_DIR} make -j${NUM_CPUS} modules_install
-${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/Image $PKG_DIR/boot/kernel7.img
+cp arch/arm/boot/dts/*.dtb $PKG_DIR/boot/
+cp arch/arm/boot/dts/overlays/*.dtb* $PKG_DIR/boot/overlays/
+cp arch/arm/boot/dts/overlays/README $PKG_DIR/boot/overlays/
+${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel7.img
 cp -r ${MOD_DIR}/lib/* ${PKG_DIR}
 
 cd $PKG_TMP
